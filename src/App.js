c71ba65bd29aaa167f51c57f5735ef70948c8ab0
Jishnu-ms <jishnums830@gmail.com>
2025-01-18 12:23:51 +0530

    Posters Section Added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import Header from "./Components/Header/Header";
 import './App.css'
 import Banner from "./Components/Header/Banner/Banner";
+import Rowpost from "./Components/RowPost/Rowpost";
 function App() {
   return (
     <div className="App">
      <Header/>
      <Banner/>
+     <Rowpost/>
     </div>
   );
 }
---
+++ src/Components/RowPost/Rowpost.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './RowPost.css'
+function Rowpost() {
+  return (
+    <div classname="row">
+      <h2>Title</h2>
+      <div className="posters">
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+        <img className='poster' src="https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.randomhouse.com/cover/d/9780593723241" alt="Poster"></img>
+      </div>
+    </div>
+  )
+}
+
+export default Rowpost
