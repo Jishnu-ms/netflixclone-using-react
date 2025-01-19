951ed9d25d0623365054841de08715d61ecf2cbe
Jishnu-ms <jishnums830@gmail.com>
2025-01-19 13:18:05 +0530

    API Integration (TMDB)

--- src/Components/Header/Banner/Banner.js
+++ src/Components/Header/Banner/Banner.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import './Banner.css';
+import axios from '../../axios'
+import {API_KEY} from '../../constants/constants'
+import { useFormState } from 'react-dom';
 function Banner() {
+  const [movie, setMovie]=useState()
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`)
+      .then((response) => {
+        console.log(response.data.results[2]); // Logs the trending movies data
+      setMovie(response.data.results[2])
+      
+      })
+      .catch((error) => {
+        console.error('Error fetching trending movies:', error);
+      });
+  }, []);
   return (
-    <div className="banner">
+    <div className="banner"
+    style={{
+      backgroundImage: movie
+        ? `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`
+        : 'none',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+    }}>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+        <h1 className="title">{movie ? movie.title : 'Loading...'}</h1>
             <div className='banner_buttons'>
                 <button className='button'>PLAY</button>
                 <button className='button'>MY_LIST</button>
             </div>
-            <h1 className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <h1 className='description'>{movie ? movie.overview : 'Loading...'}
             </h1>
         </div>
         <div className="fade_bottom"></div>
---
+++ src/Components/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseurl='https://api.themoviedb.org/3';
+
+export const API_KEY='ae1858d1f52c8ecb071faf34a8c819f9';
